08c7fee31fcd3d1f0e163320f85687e8b3697863
KlimBirukov <klimbirukov0@gmail.com>
2021-10-28 21:48:53 +0300

    fixed a bug with loading styles

--- webpack.config.js
+++ webpack.config.js
@@ -23,15 +23,10 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.css$/i,
+                test: /\.css$/,
                 use: [
                     'style-loader',
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            modules: true,
-                        }
-                    },
+                    'css-loader'
                 ]
             }
         ]
